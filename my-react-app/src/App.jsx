492a357d2958b55f5cd3d9a9a24142ad6a03b68e
tzhtet <thanzawhtet18@gmail.com>
2025-12-05 14:34:01 +0630

    render list

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,25 @@
 import List from "./List.jsx";
 
 function App() {
+
+  const fruits = [{id: 1, name: "apple", calories: 90}, 
+                    {id: 2, name: "orange", calories: 40},
+                    {id: 3, name: "banana", calories: 34},
+                    {id: 4, name: "coconut", calories: 56},
+                    {id: 5, name: "pineapple", calories: 70} 
+                    ];
+  
+  const vegetables = [{id: 6, name: "potatos", calories: 110}, 
+                      {id: 7, name: "celery", calories: 15},
+                      {id: 8, name: "carrots", calories: 44},
+                      {id: 9, name: "corn", calories: 56},
+                      {id: 10, name: "broccoli", calories: 30} 
+                    ];
+
   return(
     <>
-    <List/>
+    <List items={fruits} category="Fruits"/>
+    <List items= {vegetables} category="Vegetables"/>
     </>
   );
 }
--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,18 +1,31 @@
 
 
-function List(){
+function List(props){
 
-    const fruits = [{id: 1, name: "apple", calories: 90}, 
-                    {id: 2, name: "orange", calories: 40},
-                    {id: 3, name: "banana", calories: 34},
-                    {id: 4, name: "coconut", calories: 56},
-                    {id: 5, name: "pineapple", calories: 70} 
-                    ];
+    // const fruits = [{id: 1, name: "apple", calories: 90}, 
+    //                 {id: 2, name: "orange", calories: 40},
+    //                 {id: 3, name: "banana", calories: 34},
+    //                 {id: 4, name: "coconut", calories: 56},
+    //                 {id: 5, name: "pineapple", calories: 70} 
+    //                 ];
 
-    const listItem = fruits.map(fruit => <li key={fruit.id}>
-                                            {fruit.name}: &nbsp; 
-                                            <b>{fruit.calories}</b></li>);
-    return (<ol>{listItem}</ol>);
+    //fruits.sort((a,b) => a.name.localeCompare(b.name)); //ALPHABETICAL
+    //fruits.sort((a,b) => b.name.localeCompare(a.name));   //REVERSE ALPHABETICAL
+    //fruits.sort((a,b) => a.calories - b.calories); // NUMERIC
+    //fruits.sort((a, b) => b.calories - a.calories); 
+
+    const categtory = props.category;
+    const itemList =  props.items;
+
+    const listItems = itemList.map(item => <li key={item.id}>
+                                            {item.name}: &nbsp; 
+                                            <b>{item.calories}</b></li>);
+    return (
+        <>
+        <h3 className="list-category">{categtory}</h3>
+        <ol className="list-items">{listItems}</ol>
+        </>
+    );
 }
 
 export default List
